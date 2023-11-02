b6941c1ce3d1db757f806a940f997ed6907f91cd
tapriandi <tapriandi@gmail.com>
2023-11-02 08:53:09 +0700

    user - spatie:base role permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,20 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user1 = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+        ]);
+
+        $user2 = User::factory()->create([
+            'name' => 'Editing',
+            'email' => 'edit@mail.com'
+        ]);
+
+        $role = Role::create(['name' => 'admin']);
+        $user1->assignRole($role);
+
+        $role = Role::create(['name' => 'editing']);
+        $user2->assignRole($role);
     }
 }
